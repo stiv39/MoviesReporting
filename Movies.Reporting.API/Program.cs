aa3b7cded1e4f1ce6ba7e7edb6c0556c725046b9
stiv39 <jarsinsky.norbert@gmail.com>
2023-09-16 21:07:40 +0200

    added dockerCompose

--- Movies.Reporting.API/Program.cs
+++ Movies.Reporting.API/Program.cs
@@ -45,12 +45,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.ApplyMigrations();
-}
+ //   app.ApplyMigrations();
+
 
 app.MapCarter();
 
